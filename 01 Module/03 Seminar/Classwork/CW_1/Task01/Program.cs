b665b6b2b6c6bcaad4fbda28ca7e97b8be0eff57
cretix <nikita.amirkhanov@gmail.com>
2021-09-14 19:30:15 +0300

    classwork sem3

---
+++ 01 Module/03 Seminar/Classwork/CW_1/Task01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+
+        public static bool b(int n) => Convert.ToBoolean(n);
+        public static int c(bool n) => Convert.ToInt32(n);
+
+        public static bool Function1(bool p, bool q) => !(p & q) & !(p | !q);
+
+        public static void Function2(bool p, bool q, out bool r)
+        {
+            r = !(p & q) & !(p | !q);
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"{i} {j} => {c(Function1(b(i), b(j)))}");
+                }
+            }
+        }
+    }
+}
---
+++ 01 Module/03 Seminar/Classwork/CW_1/Task02/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+
+        public static void FunctionFor()
+        {
+            Console.WriteLine("for: ");
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write($"{i * i } ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void FunctionWhile()
+        {
+            Console.WriteLine("while: ");
+            int i = 1;
+            while (i < 11)
+            {
+                Console.Write($"{i * i } ");
+                i++;
+            }
+            Console.WriteLine();
+        }
+
+        public static void FunctionDoWhile()
+        {
+            Console.WriteLine("do while: ");
+            int i = 1;
+            do
+            {
+                Console.Write($"{i * i } ");
+                i++;
+            } while (i < 11);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            FunctionFor();
+            FunctionWhile();
+            FunctionDoWhile();
+        }
+    }
+}
