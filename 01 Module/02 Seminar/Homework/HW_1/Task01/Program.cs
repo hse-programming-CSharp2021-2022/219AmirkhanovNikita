58e6ef319e3e3623b2688310e5683688dbc603f5
cretix <nikita.amirkhanov@gmail.com>
2021-09-14 15:46:24 +0300

    task 01 done

---
+++ 01 Module/02 Seminar/Homework/HW_1/Task01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+
+        public static long calc(long x)
+        {
+            long x2 = x * x; // 3 умножения)))))
+            return (3*x2*((x2<<2) + (x+x+x) - 1) + (x<<1) - 4);
+        }
+
+
+        static void Main(string[] args)
+        {
+            long x;
+            do
+            {
+                Console.Write("Enter x: ");
+                if (!long.TryParse(Console.ReadLine(), out x))
+                {
+                    Console.WriteLine("Arg error");
+                    return;
+                }
+                Console.WriteLine($"F(x) = {calc(x)}");
+                Console.WriteLine("Press ESC to exit / ENTER to continue!");
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
